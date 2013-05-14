a8697bb8d22369c7798add8fcd613395746c5cd9
Justin Swanson <geeksunny@gmail.com>
2013-05-14 00:14:06 -0500

    Cleaning up white space.

--- NSStringNSData+Hashes.m
+++ NSStringNSData+Hashes.m
@@ -10,51 +10,43 @@
 
 #import "NSStringNSData+Hashes.h"
 
-static inline NSString *NSStringCCHashFunction(unsigned char *(function)(const void *data, CC_LONG len, unsigned char *md), CC_LONG digestLength, NSString *string)
-{
+static inline NSString *NSStringCCHashFunction(unsigned char *(function)(const void *data, CC_LONG len, unsigned char *md), CC_LONG digestLength, NSString *string) {
     NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
     uint8_t digest[digestLength];
-    
+
     function(data.bytes, (CC_LONG)data.length, digest);
-    
+
     NSMutableString *output = [NSMutableString stringWithCapacity:digestLength * 2];
-    
-    for (int i = 0; i < digestLength; i++)
-    {
+
+    for (int i = 0; i < digestLength; i++) {
         [output appendFormat:@"%02x", digest[i]];
     }
-    
+
     return output;
 }
                              
 @implementation NSString (Hashes)
 
-- (NSString *)md5
-{
+- (NSString *)md5 {
     return NSStringCCHashFunction(CC_MD5, CC_MD5_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha1
-{
+- (NSString *)sha1 {
     return NSStringCCHashFunction(CC_SHA1, CC_SHA1_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha224
-{
+- (NSString *)sha224 {
     return NSStringCCHashFunction(CC_SHA224, CC_SHA224_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha256
-{
+- (NSString *)sha256 {
     return NSStringCCHashFunction(CC_SHA256, CC_SHA256_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha384
-{
+- (NSString *)sha384 {
     return NSStringCCHashFunction(CC_SHA384, CC_SHA384_DIGEST_LENGTH, self);
 }
-- (NSString *)sha512
-{
+- (NSString *)sha512 {
     return NSStringCCHashFunction(CC_SHA512, CC_SHA512_DIGEST_LENGTH, self);
 }
 
@@ -62,32 +54,26 @@ static inline NSString *NSStringCCHashFunction(unsigned char *(function)(const v
 
 @implementation NSData (Hashes)
 
-- (NSString *)md5
-{
+- (NSString *)md5 {
     return NSStringCCHashFunction(CC_MD5, CC_MD5_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha1
-{
+- (NSString *)sha1 {
     return NSStringCCHashFunction(CC_SHA1, CC_SHA1_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha224
-{
+- (NSString *)sha224 {
     return NSStringCCHashFunction(CC_SHA224, CC_SHA224_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha256
-{
+- (NSString *)sha256 {
     return NSStringCCHashFunction(CC_SHA256, CC_SHA256_DIGEST_LENGTH, self);
 }
 
-- (NSString *)sha384
-{
+- (NSString *)sha384 {
     return NSStringCCHashFunction(CC_SHA384, CC_SHA384_DIGEST_LENGTH, self);
 }
-- (NSString *)sha512
-{
+- (NSString *)sha512{
     return NSStringCCHashFunction(CC_SHA512, CC_SHA512_DIGEST_LENGTH, self);
 }
 
