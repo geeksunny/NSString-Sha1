bbd0752fb410fcf2f50a3e437043066cc5bfd7ec
Justin Swanson <geeksunny@gmail.com>
2013-05-13 23:56:44 -0500

    Adding NSData support.

--- NSStringNSData+Hashes.h
+++ NSStringNSData+Hashes.h
@@ -1,13 +1,25 @@
 //
-//  NSString+Hashes.h
+//  NSStringNSData+Hashes.h
 //
 //  Created by Klaus-Peter Dudas on 26/07/2011.
+//  NSData adapted from NSString+Hashes by Justin Swanson on 02/25/2013.
 //  Copyright: Do whatever you want with this, i.e. Public Domain
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSString (Hashes)
+
+@property (nonatomic, readonly) NSString *md5;
+@property (nonatomic, readonly) NSString *sha1;
+@property (nonatomic, readonly) NSString *sha224;
+@property (nonatomic, readonly) NSString *sha256;
+@property (nonatomic, readonly) NSString *sha384;
+@property (nonatomic, readonly) NSString *sha512;
+
+@end
+
+@interface NSData (Hashes)
 
 @property (nonatomic, readonly) NSString *md5;
 @property (nonatomic, readonly) NSString *sha1;
--- NSStringNSData+Hashes.m
+++ NSStringNSData+Hashes.m
@@ -1,13 +1,14 @@
 //
-//  NSString+Hashes.m
+//  NSStringNSData+Hashes.m
 //
 //  Created by Klaus-Peter Dudas on 26/07/2011.
+//  NSData adapted from NSString+Hashes by Justin Swanson on 02/25/2013.
 //  Copyright: Do whatever you want with this, i.e. Public Domain
 //
 
 #import <CommonCrypto/CommonDigest.h>
 
-#import "NSString+Hashes.h"
+#import "NSStringNSData+Hashes.h"
 
 static inline NSString *NSStringCCHashFunction(unsigned char *(function)(const void *data, CC_LONG len, unsigned char *md), CC_LONG digestLength, NSString *string)
 {
@@ -27,6 +28,39 @@ static inline NSString *NSStringCCHashFunction(unsigned char *(function)(const v
 }
                              
 @implementation NSString (Hashes)
+
+- (NSString *)md5
+{
+    return NSStringCCHashFunction(CC_MD5, CC_MD5_DIGEST_LENGTH, self);
+}
+
+- (NSString *)sha1
+{
+    return NSStringCCHashFunction(CC_SHA1, CC_SHA1_DIGEST_LENGTH, self);
+}
+
+- (NSString *)sha224
+{
+    return NSStringCCHashFunction(CC_SHA224, CC_SHA224_DIGEST_LENGTH, self);
+}
+
+- (NSString *)sha256
+{
+    return NSStringCCHashFunction(CC_SHA256, CC_SHA256_DIGEST_LENGTH, self);
+}
+
+- (NSString *)sha384
+{
+    return NSStringCCHashFunction(CC_SHA384, CC_SHA384_DIGEST_LENGTH, self);
+}
+- (NSString *)sha512
+{
+    return NSStringCCHashFunction(CC_SHA512, CC_SHA512_DIGEST_LENGTH, self);
+}
+
+@end
+
+@implementation NSData (Hashes)
 
 - (NSString *)md5
 {
