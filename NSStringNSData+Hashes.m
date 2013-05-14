47f55cd045b906df612aaa0994e2697e4d91e1dc
Justin Swanson <geeksunny@gmail.com>
2013-05-14 00:25:27 -0500

    Re-implemented @onlinecity's 'c-loop and initWithBytesNoCopy:length:encoding:freeWhenDone'.

--- NSStringNSData+Hashes.m
+++ NSStringNSData+Hashes.m
@@ -2,6 +2,7 @@
 //  NSStringNSData+Hashes.m
 //
 //  Created by Klaus-Peter Dudas on 26/07/2011.
+//  Modified by hd@onlinecity.dk to not use NSMutableString which is (kinda) slow.
 //  NSData adapted from NSString+Hashes by Justin Swanson on 02/25/2013.
 //  Copyright: Do whatever you want with this, i.e. Public Domain
 //
@@ -16,13 +17,22 @@ static inline NSString *NSStringCCHashFunction(unsigned char *(function)(const v
 
     function(data.bytes, (CC_LONG)data.length, digest);
 
-    NSMutableString *output = [NSMutableString stringWithCapacity:digestLength * 2];
+    // Convert to hex (high performance)
+    static const char HexEncodeChars[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    char *resultData;
+    resultData = malloc(digestLength*2+1);
 
-    for (int i = 0; i < digestLength; i++) {
-        [output appendFormat:@"%02x", digest[i]];
+    for (uint i = 0; i < digestLength; i++) {
+        resultData[i*2]=HexEncodeChars[(digest[i] >> 4)];
+        resultData[i*2+1]=HexEncodeChars[(digest[i] % 0x10)];
     }
+    resultData[digestLength*2] = 0;
 
-    return output;
+    // Return as a NSString without copying the bytes
+    return [[NSString alloc] initWithBytesNoCopy:resultData
+                                          length:digestLength*2
+                                        encoding:NSASCIIStringEncoding
+                                    freeWhenDone:YES];
 }
                              
 @implementation NSString (Hashes)
